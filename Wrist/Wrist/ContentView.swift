45309334d1f25e59b6dc34a0f947fde921b7eb9d
crying-goose <newability.master@gmail.com>
2023-03-27 01:12:01 +0900

    프로젝트 init

---
+++ Wrist/Wrist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Wrist
+//
+//  Created by 라주영 on 2023/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("알빠노")
+            Button("State 바꾸기") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wrist/Wrist/WristApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristApp.swift
+//  Wrist
+//
+//  Created by 라주영 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
